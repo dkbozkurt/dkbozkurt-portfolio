ca8f0ab7b3c3fc9b9cf080d18f84476ee5665f79
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-01-12 17:26:46 +0100

    Experience section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
+import Experience from "@/components/experience";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <About />
         <Projects />
         <Skills />
+        <Experience />
     </main>
   )
 }
---
+++ components/experience.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React from 'react'
+import SectionHeading from './section-heading'
+import { VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import { experiencesData } from '@/lib/data';
+
+export default function Experience() {
+  return (
+    <section
+    id="experience"
+    >
+        <SectionHeading>My Experience</SectionHeading>
+        <VerticalTimeline lineColor="">
+            {
+                experiencesData.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <VerticalTimelineElement
+                            contentStyle ={{
+                                background: "#f3f4f6",
+                                boxShadow: "none",
+                                border: "1px solid rgba(0, 0, 0, 0.05)",
+                                textAlign: "left",
+                                padding: "1.3rem, 2rem",
+                            }}
+                            contentArrowStyle={{
+                                borderRight: "0.4rem solid #9ca3af",
+                            }}
+                            date={item.date}
+                            icon={item.icon}
+                            iconStyle={{
+                                background: "white",
+                                fontSize: "1.5rem",
+                            }}
+                        >
+                            <h3
+                            className="font-semibold capitalize">
+                                {item.title}
+                            </h3>
+                            <p
+                            className="font-normal !mt-0"
+                            >
+                                {item.location}
+                            </p>
+                            <p
+                            className ="!mt-1 !font-normal text-gray-700">
+                                {item.description}
+                            </p>
+                        </VerticalTimelineElement>
+                    </React.Fragment>
+                ))
+            }
+        </VerticalTimeline>
+
+    </section>
+  )
+}
