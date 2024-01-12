131d417cc8ae6be34718d95d5ebce9386fb566db
Dogukan Bozkurt <dogukan@justdice.io>
2024-01-12 14:25:48 +0100

    Projects added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <Intro />
         <SectionDivider />
         <About />
+        <Projects />
     </main>
   )
 }
--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -7,7 +7,7 @@ export default function SectionHeading({
 children }: SectionHeadingProps) {
   return (
     <h2 
-    className = "mb-8 text-3xl font-medium capitalize">
+    className = "mb-8 text-3xl font-medium text-center capitalize">
         {children}
     </h2>
   )
