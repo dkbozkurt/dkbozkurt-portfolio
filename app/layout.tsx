2f2d397e905319cc0526578b2be2a59245cb60f9
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-01-12 15:49:43 +0100

    refactor active section context to detect location of viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import ActiveSectionContextProvider from '@/context/active-section-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,9 +22,11 @@ export default function RootLayout({
         <body className={`${inter.className} bg-slate-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
             <div className = "bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
             <div className = "bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-
-            <Header />
-            {children}
+            
+            <ActiveSectionContextProvider>
+                <Header />
+                {children}
+            </ActiveSectionContextProvider>
         </body>
         </html>
     )
