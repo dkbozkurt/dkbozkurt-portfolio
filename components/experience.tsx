513c02c266ac7b22416028d9aac00916af4f75fc
dkbozkurt <dkaanbozkurt@gmail.com>
2024-01-12 20:55:54 +0100

    experience bug FIXED

--- components/experience.tsx
+++ components/experience.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import SectionHeading from './section-heading'
 import { VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
 
