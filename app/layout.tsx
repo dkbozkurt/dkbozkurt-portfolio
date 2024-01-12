8e9b5789e8c353f94bc0636bafd85fa0324e34f7
dkbozkurt <dkaanbozkurt@gmail.com>
2024-01-12 01:11:09 +0100

    background blur colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,23 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dogukan Kaan Bozkurt | Personal Portfolio',
-  description: 'Playable Ads/ Web developer with 3 years of experience/',
+    title: 'Dogukan Kaan Bozkurt | Personal Portfolio',
+    description: 'Playable Ads/ Web developer with 3 years of experience/',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  )
+    children,
+    }: {
+    children: React.ReactNode
+    })
+{
+    return (
+        <html lang="en">
+        <body className={`${inter.className} bg-slate-50 text-gray-950 relative`}>
+            <div className = "bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+            <div className = "bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+            {children}
+        </body>
+        </html>
+    )
 }
