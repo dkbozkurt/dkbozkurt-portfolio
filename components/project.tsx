2139c34c1afdf5436fef2cb791bbf4d1d4f05de3
dkbozkurt <dkaanbozkurt@gmail.com>
2024-01-13 16:41:34 +0100

    mobile projects bug fix

--- components/project.tsx
+++ components/project.tsx
@@ -32,10 +32,10 @@ export default function Project({
     className="mb-3 group sm:mb-8 last:mb-0"
     >
         <section 
-        className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 rounded-lg"
+        className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 rounded-lg"
         >
             <div
-            className="px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]"
+            className="px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]"
             >
                 <h3
                 className = "text-2xl font-semibold"
@@ -64,7 +64,7 @@ export default function Project({
             src={imageUrl} 
             alt="Project Image" 
             quality={95}
-            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition 
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition 
                     
             group-hover:scale-[1.04] 
             group-hover:-translate-x-3 
