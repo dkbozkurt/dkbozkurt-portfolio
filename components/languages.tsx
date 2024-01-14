ebc8b3ac7338c0368be4c9ad1d2a0dfe47a0d355
dkbozkurt <dkaanbozkurt@gmail.com>
2024-01-14 15:27:17 +0100

    data update

---
+++ components/languages.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Languages() {
+  return (
+    <section>
+        <SectionHeading>Languages</SectionHeading>
+    </section>
+  )
+}
