c2c947dfb2afaa5b17c2bd617b07f784d3271b4c
dkbozkurt <dkaanbozkurt@gmail.com>
2024-01-22 00:04:19 +0100

    Static HTML bug fix for deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+*/
 const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-        ],
+        unoptimized: true,
     },
     experimental: {
         serverActions: true,
