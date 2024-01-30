dabe815d9944b1ada7af6ae8352363f00615c92c
dkbozkurt <dkaanbozkurt@gmail.com>
2024-01-31 00:09:34 +0100

    header fixes for lg and xll

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     <header className="z-[999] relative">
         <motion.div className ="fixed top-0 left-1/2 h-[6.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]
         sm:top-6 sm:h-[5.25rem] sm:w-[36rem] sm:rounded-full
-        lg:h-[5.25rem] lg:w-[60rem]
+        lg:h-[5.25rem] lg:w-[54rem]
         xl:h-[3.25rem] xl:w-[82rem]"
             initial={{y: -100, x: "-50%", opacity: 0}}
             animate={{y: 0,  x: "-50%", opacity: 1}}
@@ -22,11 +22,14 @@ export default function Header() {
 
         <nav className="flex fixed top-[0.15rem] h-16 left-1/2  py-2 w-[24rem]
         sm:top-[1.7rem] sm:h-12 sm:py-0 sm:w-[34rem] sm:-translate-x-1/2
-        lg:top-[1.7rem] lg:h-[initial] lg:py-0 lg:w-[63rem] lg:-translate-x-[39.5rem]
+        lg:h-[initial] lg:w-[56rem] lg:-translate-x-1/2
+        xl:h-[initial] xl:w-[63rem] xl:-translate-x-[39.5rem]
         ">
             <ul className ="flex w-[30rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 gap-x-0
             sm:w-[initial] sm:gap-x-0
-            lg:flex-nowrap lg:gap-[2rem]">
+            lg:w-[initial] lg:gap-[2rem] lg:gap-y-0 lg:h-[3.25rem]
+            xl:flex-nowrap xl:gap-[2rem] xl:h-[2.75rem]
+            ">
                 {
                     links.map(link => (
                         <motion.li
