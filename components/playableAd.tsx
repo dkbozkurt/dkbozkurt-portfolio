902e6fafbe3b9c24bb0d8b5ba4f831b89b4b0dc4
Dogukan Bozkurt <dogukan@justdice.io>
2024-02-05 10:35:13 +0100

    spaces fixed

--- components/playableAd.tsx
+++ components/playableAd.tsx
@@ -22,16 +22,14 @@ export default function PlyAd({
     useEffect(() => {
         // Disable scrolling when the overlay is visible
         if (isOverlayVisible) {
-          document.body.style.overflow = 'hidden';
+            document.body.style.overflow = 'hidden';
         } else {
-          document.body.style.overflow = 'visible';
+            document.body.style.overflow = 'visible';
         }
-    
-        // Cleanup effect
         return () => {
-          document.body.style.overflow = 'visible';
+            document.body.style.overflow = 'visible';
         };
-      }, [isOverlayVisible]);
+    }, [isOverlayVisible]);
 
     const handleClick = (targetURL:string) => {
         setOverlayVisible(!isOverlayVisible);
@@ -43,94 +41,93 @@ export default function PlyAd({
 
     const handleClose = () => {
         setOverlayVisible(false);
-      };
-      
-      return (
+    };
+
+    return (
         <a onClick={() => handleClick(url)}>
-          <motion.div ref={ref} className="mx-[1rem] group sm:mb-8 last:mb-0">
-            <section className="bg-gray-100 border border-black/5 overflow-hidden hover:bg-gray-200 transition cursor-pointer rounded-lg flex flex-col items-center w-[16rem] h-[16rem]">
-              <Image
-                src={icon}
-                alt="Language Image"
-                quality={95}
-                className="rounded-[2rem] transition flex justify-center group-hover:scale-[1.1] shadow-2xl m-5 relative h-[8rem] w-[8rem] rounded-m mt-3 mb-2 mr-5"
-              />
+            <motion.div ref={ref} className="mx-[1rem] group sm:mb-8 last:mb-0">
+                <section className="bg-gray-100 border border-black/5 overflow-hidden hover:bg-gray-200 transition cursor-pointer rounded-lg flex flex-col items-center w-[16rem] h-[16rem]">
+                    <Image
+                        src={icon}
+                        alt="Playable icon"
+                        quality={95}
+                        className="rounded-[2rem] transition flex justify-center group-hover:scale-[1.1] shadow-2xl m-5 relative h-[8rem] w-[8rem] rounded-m mt-3 mb-2 mr-5"
+                    />
     
-              <div className="flex flex-col items-center pb-3 mt-auto">
-                <h3 className="text-2xl font-bold">{appName}</h3>
-                <p className="pb-1 text-gray-700">{playableName}</p>
-                <div className="transition items-center flex w-[11rem] h-2 gap-1 pl-8 p-4 text-white text-m bg-gray-900 rounded-full outline-none sm:w-[12rem]">
-                  Click to Play
-                  <BsArrowRight className="transition opacity-70 group-hover:translate-x-2" />
+                <div className="flex flex-col items-center pb-3 mt-auto">
+                    <h3 className="text-2xl font-bold">{appName}</h3>
+                    <p className="pb-1 text-gray-700">{playableName}</p>
+                    <div className="transition items-center flex w-[11rem] h-2 gap-1 pl-8 p-4 text-white text-m bg-gray-900 rounded-full outline-none sm:w-[12rem]">
+                        Click to Play
+                        <BsArrowRight className="transition opacity-70 group-hover:translate-x-2" />
+                    </div>
                 </div>
-              </div>
-            </section>
-          </motion.div>
-    
-          {isOverlayVisible && (
+                </section>
+            </motion.div>
+
+        {isOverlayVisible && (
             <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 9998 }}>
-              {/* Semi-transparent overlay */}
-              <div
+                <div
                 style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100%',
-                  height: '100%',
-                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                  zIndex: 9998,
-                }}
-              ></div>
-    
-              {/* Content inside the overlay */}
-              <div
-                style={{
-                  position: 'fixed',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  width: '405px',
-                  height: '720px',
-                  backgroundColor: 'white',
-                  zIndex: 9999,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  borderRadius: '16px', // Adjust as needed
-                  border: '4px solid white', // Thicker and white border
-                }}
-              >
-                <button
-                  style={{
                     position: 'absolute',
-                    top: '-20px', // Adjust to move the button outside the pop-up
-                    right: '-20px', // Adjust to move the button outside the pop-up
-                    background: 'white',
-                    border: '4px solid white', // Border color matching the pop-up border
-                    borderRadius: '50%',
-                    cursor: 'pointer',
-                    width: '40px',
-                    height: '40px',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    zIndex: 9998,
+                }}
+                ></div>
+
+                <div
+                style={{
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '405px',
+                    height: '720px',
+                    backgroundColor: 'white',
+                    zIndex: 9999,
                     display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', // Optional shadow
-                  }}
-                  onClick={handleClose}
+                    flexDirection: 'column',
+                    borderRadius: '16px', // Adjust as needed
+                    border: '4px solid white', // Thicker and white border
+                }}
                 >
-                  <span style={{ color: 'black', fontSize: '20px' }}>X</span>
-                </button>
-                {/* Display HTML content from the given URL using an iframe */}
-                <iframe
-                  title="Popup Content"
-                  src={url}
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  style={{ borderRadius: '16px' }}
-                />
-              </div>
+
+                    <button
+                        style={{
+                            position: 'absolute',
+                            top: '-20px', // Adjust to move the button outside the pop-up
+                            right: '-20px', // Adjust to move the button outside the pop-up
+                            background: 'white',
+                            border: '4px solid white', // Border color matching the pop-up border
+                            borderRadius: '50%',
+                            cursor: 'pointer',
+                            width: '40px',
+                            height: '40px',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', // Optional shadow
+                        }}
+                        onClick={handleClose}
+                    >
+                        <span style={{ color: 'black', fontSize: '20px' }}>X</span>
+                    </button>
+
+                    <iframe
+                        title="Popup Content"
+                        src={url}
+                        width="100%"
+                        height="100%"
+                        frameBorder="0"
+                        style={{ borderRadius: '16px' }}
+                    />
+                </div>
             </div>
-          )}
+            )}
         </a>
-      );
+    );
 }
