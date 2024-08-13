a7734180a7e92d1cfe8306506fbe31e268786cee
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-08-13 12:28:14 +0200

    Bullet point structure integration for experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client'
 
 import React from 'react'
 import SectionHeading from './section-heading'
-import { VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
@@ -11,49 +11,56 @@ export default function Experience() {
     const { ref } = useSectionInView("Experience");
   
     return (
-    <section
-    id="experience"
-    ref = {ref} 
-    className = "scroll-mt-28 mb-28 sm:mb-40"
-    >
-        <SectionHeading>Experience</SectionHeading>
-        <VerticalTimeline lineColor="">
-            {experiencesData.map((item, index) => (
-                <React.Fragment key={index}>
-                    <VerticalTimelineElement
-                        contentStyle ={{
-                            background: "#f3f4f6",
-                            boxShadow: "none",
-                            border: "1px solid rgba(0, 0, 0, 0.05)",
-                            textAlign: "left",
-                            padding: "1.3rem, 2rem",
-                        }}
-                        contentArrowStyle={{
-                            borderRight: "0.4rem solid #9ca3af",
-                        }}
-                        date={item.date}
-                        icon={item.icon}
-                        iconStyle={{
-                            background: "white",
-                            fontSize: "1.5rem",
-                        }}
-                    >
-                        <h3 className="text-lg font-semibold capitalize">
-                            {item.title}
-                        </h3>
-                        <p className="font-normal !mt-0"
+        <section
+            id="experience"
+            ref={ref}
+            className="scroll-mt-28 mb-28 sm:mb-40"
+        >
+            <SectionHeading>Experience</SectionHeading>
+            <VerticalTimeline lineColor="">
+                {experiencesData.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <VerticalTimelineElement
+                            contentStyle={{
+                                background: "#f3f4f6",
+                                boxShadow: "none",
+                                border: "1px solid rgba(0, 0, 0, 0.05)",
+                                textAlign: "left",
+                                padding: "1.3rem, 2rem",
+                            }}
+                            contentArrowStyle={{
+                                borderRight: "0.4rem solid #9ca3af",
+                            }}
+                            date={item.date}
+                            icon={item.icon}
+                            iconStyle={{
+                                background: "white",
+                                fontSize: "1.5rem",
+                            }}
                         >
-                            {item.company}
-                            {" "}<span>&#8226;</span>{" "}
-                            {item.location}
-                        </p>
-                        <p className ="!mt-1 !font-normal text-gray-700">
-                            {item.description}
-                        </p>
-                    </VerticalTimelineElement>
-                </React.Fragment>
-            ))}
-    </VerticalTimeline>
-    </section>
-  )
+                            <h3 className="text-lg font-semibold capitalize">
+                                {item.title}
+                            </h3>
+                            <p className="font-normal !mt-0">
+                                {item.company}
+                                {" "}<span>&#8226;</span>{" "}
+                                {item.location}
+                            </p>
+                            {Array.isArray(item.description) ? (
+                                <ul className="list-disc ml-5 !mt-1 !font-normal text-gray-700">
+                                    {item.description.map((desc, i) => (
+                                        <li key={i}>{desc}</li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p className="!mt-1 !font-normal text-gray-700">
+                                    {item.description}
+                                </p>
+                            )}
+                        </VerticalTimelineElement>
+                    </React.Fragment>
+                ))}
+            </VerticalTimeline>
+        </section>
+    );
 }
