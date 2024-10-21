7e915e11726173ad6d45dbc573e054096f17bc86
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-10-21 17:23:13 +0200

    card animation update

--- components/playableAd.tsx
+++ components/playableAd.tsx
@@ -5,8 +5,26 @@ import { playableAdsData } from "@/lib/data";
 import Image from 'next/image'
 import { motion } from "framer-motion"
 import { BsArrowRight } from "react-icons/bs";
+import { FaStar } from "react-icons/fa";
 
 type PlayableAdsProps = typeof playableAdsData[number];
+
+const StarAnimation = ({ delay }: { delay: number }) => {
+    const randomScale = Math.random() * 0.5 + 0.5; // Random scale between 0.5 and 1
+    return (
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: [0, 1, 0], scale: [0, randomScale, 0] }}
+            transition={{
+                duration: 2,
+                repeat: Infinity,
+                delay: delay,
+            }}
+        >
+            <FaStar className="text-yellow-400 text-8xl" />
+        </motion.div>
+    );
+};
 
 export default function PlayableAd({
     appName,
@@ -17,10 +35,9 @@ export default function PlayableAd({
 }: PlayableAdsProps) {
     const [isOverlayVisible, setOverlayVisible] = useState(false);
     const ref = useRef<HTMLDivElement>(null);
-    
-    const cardClasses = `bg-gray-100 border border-black/5 overflow-hidden hover:bg-gray-200 transition cursor-pointer rounded-lg flex flex-col items-center w-[16rem] h-[16rem] ${
-        isHighlighted ? "bg-yellow-200 hover:bg-yellow-300 relative" : ""
-    }`;
+
+    const cardClasses = `bg-gray-100 border border-black/5 overflow-hidden hover:bg-gray-200 transition cursor-pointer rounded-lg flex flex-col items-center w-[16rem] h-[16rem] ${isHighlighted ? "bg-yellow-200 hover:bg-yellow-300 relative" : ""
+        }`;
 
     useEffect(() => {
         if (isOverlayVisible) {
@@ -54,10 +71,24 @@ export default function PlayableAd({
         >
             <div className={cardClasses} onClick={() => handleClick(url)}>
                 {isHighlighted && (
-                    <div className="absolute inset-0 z-0 overflow-hidden rounded-lg">
-                        <div className="absolute inset-0 bg-yellow-300 opacity-20"></div>
-                        <div className="absolute inset-0 animate-[spin_3s_linear_infinite] bg-gradient-to-r from-transparent via-white to-transparent opacity-30"></div>
-                    </div>
+                    <>
+                        <div className="absolute inset-0 z-0 overflow-hidden rounded-lg">
+                            <div className="absolute inset-0 bg-yellow-300 opacity-20"></div>
+                            <div className="absolute inset-0 animate-[spin_3s_linear_infinite] bg-gradient-to-r from-transparent via-white to-transparent opacity-30"></div>
+                        </div>
+                        <div className="absolute -top-2 -left-2 z--20">
+                            <StarAnimation delay={0} />
+                        </div>
+                        <div className="absolute -top-2 -right-2 z--20">
+                            <StarAnimation delay={0.5} />
+                        </div>
+                        <div className="absolute -bottom-2 -left-2 z--20">
+                            <StarAnimation delay={1} />
+                        </div>
+                        <div className="absolute -bottom-2 -right-2 z--20">
+                            <StarAnimation delay={1.5} />
+                        </div>
+                    </>
                 )}
                 <Image
                     src={icon}
@@ -65,7 +96,7 @@ export default function PlayableAd({
                     quality={95}
                     className="rounded-[2rem] transition flex justify-center group-hover:scale-[1.1] shadow-2xl m-5 relative h-[8rem] w-[8rem] rounded-m mt-3 mb-2 mr-5 z-10"
                 />
-    
+
                 <div className="flex flex-col items-center pb-3 mt-auto z-10">
                     <h3 className="text-2xl font-bold">{appName}</h3>
                     <p className="pb-1 text-gray-700">{playableName}</p>
@@ -77,46 +108,20 @@ export default function PlayableAd({
             </div>
 
             {isOverlayVisible && (
-                <div className="fixed top-0 left-0 w-full h-full" style={{ zIndex: 9998 }}>
-                    <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"
-                    style={{ zIndex: 9998 }}
-                    ></div>
-
-                    <div className="fixed z-50 flex flex-col items-center justify-center transform -translate-x-1/2 -translate-y-1/2 bg-white border-white top-1/2 left-1/2"
-                    style={{
-                        width: '405px',
-                        height: '720px',
-                        zIndex: 9999,
-                        borderRadius: '16px',
-                        border: '6px solid white',
-                    }}
-                    >
-                        <button className="absolute flex items-center justify-center bg-white shadow-md cursor-pointer"
-                            style={{
-                                top: '-20px',
-                                right: '-20px',
-                                background: 'white',
-                                border: '4px solid white',
-                                borderRadius: '50%',
-                                width: '40px',
-                                height: '40px',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
-                            }}
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <div className="absolute inset-0 bg-black bg-opacity-50" onClick={handleClose}></div>
+                    <div className="relative bg-white rounded-lg shadow-xl" style={{ width: '405px', height: '720px' }}>
+                        <button
+                            className="absolute -top-4 -right-4 w-8 h-8 flex items-center justify-center bg-white rounded-full shadow-md text-gray-600 hover:text-gray-800"
                             onClick={handleClose}
                         >
-                            <span className="font-black text-[20px]">X</span>
+                            <span className="text-2xl">&times;</span>
                         </button>
-
                         <iframe
                             title="Popup Content"
                             src={url}
-                            width="100%"
-                            height="100%"
+                            className="w-full h-full rounded-lg"
                             frameBorder="0"
-                            style={{ borderRadius: '16px' }}
                         />
                     </div>
                 </div>
